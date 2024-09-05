83d17d907083bbcb9fb973d67dfa8d186888f1db
Jacky Liu <qsliu@google.com>
2024-09-05 14:57:28 +0800

    bluejay: Setup bluejay build target
    Setup bluejay build target on android14-gs-pixel-6.1.
    
    Bug: 358264236
    Change-Id: I58846a0a8400c007751ee09963095a2b66a9e09b
    Signed-off-by: Jacky Liu <qsliu@google.com>

--- build_bluejay.sh
+++ build_bluejay.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 # SPDX-License-Identifier: GPL-2.0
-DEVICE_KERNEL_BUILD_CONFIG=private/devices/google/bluejay/build.config.bluejay \
-private/gs-google/build_slider.sh "$@"
+
+exec tools/bazel run \
+    --config=stamp \
+    --config=bluejay \
+    //private/devices/google/bluejay:gs101_bluejay_dist "$@"
